42c14329ce8b6e8a3797ce559be38c2e208acea6
JoeyJohnJo <joeyjohnjo@gmail.com>
2023-04-09 11:55:38 -0300

    adicionado documentacao

--- src/main/kotlin/Constantes.kt
+++ src/main/kotlin/Constantes.kt
@@ -1,5 +1,19 @@
+/**
+ * Um objeto que contém constantes para as opções de navegação do aplicativo.
+ */
 object Nav {
+    /**
+     * Uma constante para a opção "Listar quizzes existentes".
+     */
     const val listarQuizzes = "1"
+
+    /**
+     * Uma constante para a opção "Criar novo quiz".
+     */
     const val criarNovoQuiz = "2"
+
+    /**
+     * Uma constante para a opção "Fechar aplicativo".
+     */
     const val fecharAplicativo = "X"
 }
--- src/main/kotlin/telas/abrirMenuInicial.kt
+++ src/main/kotlin/telas/abrirMenuInicial.kt
@@ -1,5 +1,8 @@
 package telas
 
+/**
+ * Exibe o menu inicial do aplicativo e permite ao usuário navegar pelas opções.
+ */
 fun abrirMenuInicial() {
     println("\n\n--------------- GERADOR DE QUIZ ---------------")
 
@@ -9,21 +12,26 @@ fun abrirMenuInicial() {
         println("${Nav.criarNovoQuiz} - Criar novo quiz")
         println("${Nav.fecharAplicativo} - Fechar aplicativo")
 
+        // Aguarda a seleção do usuário e navega para a opção correspondente
         if (navegarParaOpcaoSelecionada()) break
 
     }
 }
 
+/**
+ * Lê a entrada do usuário e navega para a opção correspondente.
+ * @return true se o usuário selecionou a opção "Fechar aplicativo", false caso contrário.
+ */
 private fun navegarParaOpcaoSelecionada(): Boolean {
     val opcaoSelecionada = readln()
 
     if (opcaoSelecionada == Nav.listarQuizzes)
-        abrirListaDeQuiz()
+        abrirListaDeQuiz() // Exibe a lista de quizzes existentes
     else if (opcaoSelecionada == Nav.criarNovoQuiz)
-        abrirMenuCriarNovoQuiz()
+        abrirMenuCriarNovoQuiz() // Exibe o menu para criar um novo quiz
     else if (opcaoSelecionada.lowercase() == Nav.fecharAplicativo.lowercase()) {
-        sairPrograma()
+        sairPrograma() // Fecha o aplicativo
         return true
-    } else opcaoNaoReconhecida()
+    } else opcaoNaoReconhecida() // Exibe uma mensagem de erro para opções inválidas
     return false
 }
